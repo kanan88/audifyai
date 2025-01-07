92527bc40640b430ee753dd9dc543172c462576d
Kanan Farzaliyev <kanan.farzali@gmail.com>
2025-01-07 02:29:40 +0100

    implement dummy homepage structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,7 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -10,8 +13,25 @@ export default function RootLayout({
       <main className="relative flex bg-black-3">
         <LeftSidebar />
 
-        {children}
-        <p className="text-white-1">RIGHT SIDEBAR</p>
+        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image
+                src="/icons/logo.svg"
+                alt="menu icon"
+                width={30}
+                height={30}
+              />
+              <MobileNav />
+            </div>
+            <div className="fle flex-col md:pb-14">
+              Toaster (notification popups)
+              {children}
+            </div>
+          </div>
+        </section>
+
+        <RightSidebar />
       </main>
     </div>
   );
